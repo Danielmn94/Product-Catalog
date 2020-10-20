931e22ef78e5461c1d251171cacd2fc3d27cfc9e
Daniel <danielmlgaard@hotmail.com>
2020-10-20 10:19:43 +0200

    DB Model created

---
+++ Product_Catalog.Data/Domain/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product_Catalog.Data.Domain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string StyleName { get; set; }
+        public string Description { get; set; }
+        public string ColorName { get; set; }
+        public string SizeName { get; set; }
+        public bool Sustainable { get; set; }
+        public string ItemNo { get; set; }
+    }
+}
